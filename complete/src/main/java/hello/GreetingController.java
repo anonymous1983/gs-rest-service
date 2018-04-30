0efc7207400160ea93b1c5554d3e689c0af4e414
ANIS ABID <anis.abid@atexo.com>
2018-04-30 17:38:57 +0200

    add Map /user/all and fixe bug /user/add

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -1,22 +1,18 @@
 package hello;
 
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path="/api/v1")
+@RequestMapping(path = "/api/v1")
 public class GreetingController {
     @Autowired
     private UserRepository userRepository;
 
 
-
-    @GetMapping(path="/user/add") // Map ONLY GET Requests
+    @GetMapping(path = "/user/add") // Map ONLY GET Requests
     public @ResponseBody
-    String addNewUser (@RequestParam String name
+    String addNewUser(@RequestParam String name
             , @RequestParam String email) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
@@ -27,5 +23,15 @@ public class GreetingController {
         userRepository.save(n);
         return "Saved";
     }
+
+
+    @GetMapping(path = "/user/all") // Map ONLY GET Requests
+    public @ResponseBody
+    Iterable<User> getAllUsers() {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+        return userRepository.findAll();
+    }
+
 
 }
