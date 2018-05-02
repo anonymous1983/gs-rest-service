74e8f6321e20fae2e134d9115c33f7cd355ad95c
ANIS ABID <anis.abid@atexo.com>
2018-05-02 11:06:07 +0200

    Update PostMapping /user with RequestBody JSON

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -10,9 +10,9 @@ public class GreetingController {
     private UserRepository userRepository;
 
 
-    @GetMapping(path = "/user/add") // Map ONLY GET Requests
+    @PostMapping(path = "/user") // Map ONLY GET Requests
     public @ResponseBody
-    String addNewUser(@RequestParam String name
+    /*String addNewUser(@RequestParam String name
             , @RequestParam String email) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
@@ -22,10 +22,21 @@ public class GreetingController {
         n.setEmail(email);
         userRepository.save(n);
         return "Saved";
+    }*/
+
+    String addNewUser(@RequestBody User user) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        User n = new User();
+        n.setName(user.getName());
+        n.setEmail(user.getEmail());
+        userRepository.save(n);
+        return "Saved";
     }
 
 
-    @GetMapping(path = "/user/all") // Map ONLY GET Requests
+    @GetMapping(path = "/user") // Map ONLY GET Requests
     public @ResponseBody
     Iterable<User> getAllUsers() {
         // @ResponseBody means the returned String is the response, not a view name
