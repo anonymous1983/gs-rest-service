c8e2e1d694a3b25497399d540e7f46c53bec2e92
ANIS ABID <anis.abid@atexo.com>
2018-05-02 12:00:40 +0200

    update architecture /front /back /server /statics

---
+++ server/application/src/main/java/com/commun/application/UserController.java
@@ -0,0 +1,48 @@
+package com.commun.application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @PostMapping(path = "/user") // Map ONLY GET Requests
+    public @ResponseBody
+    /*String addNewUser(@RequestParam String name
+            , @RequestParam String email) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+        userRepository.save(n);
+        return "Saved";
+    }*/
+
+    String addNewUser(@RequestBody User user) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        User n = new User();
+        n.setName(user.getName());
+        n.setEmail(user.getEmail());
+        userRepository.save(n);
+        return "Saved";
+    }
+
+
+    @GetMapping(path = "/user") // Map ONLY GET Requests
+    public @ResponseBody
+    Iterable<User> getAllUsers() {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+        return userRepository.findAll();
+    }
+
+
+}
